f41a9525e698298ff8ddc81926a6068b702abb91
localhost <elmehdi.asr@gmail.com>
2022-01-14 02:32:27 +0100

    add details function in service with some styles

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-details',
@@ -6,11 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
-  gameRating = 10
+  gameRating = 0;
+  gameId!: number;
+  game!: Game;
+  routeSubscription!: Subscription;
+  gameServiceSubscription!: Subscription;
 
-  constructor() { }
+  constructor(
+    private httpService: HttpService,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.routeSubscription = this.activatedRoute.params.subscribe((params: Params) => {
+      this.gameId = params['id'];
+      this.getGameDetails(this.gameId);
+;    })
+  }
+  getGameDetails(gameId: number) {
+    this.gameServiceSubscription = this.httpService.getGameDetails(gameId).subscribe((response: Game) => {
+      this.game = response;
+
+      setTimeout(() => {
+        this.gameRating = this.game.metacritic;
+      }, 1000);
+    })
   }
 
   getColor(value: number): string {
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { forkJoin, map, Observable } from 'rxjs';
 import { environment as env } from 'src/environments/environment';
 import { APIResponse, Game } from '../models';
 @Injectable({
@@ -19,4 +19,29 @@ export class HttpService {
       params: params
     });
   }
+
+  getGameDetails(id: number): Observable<Game> {
+    const gameInfoRequest = this.http.get(`${env.BASE_URL}/games/${id}`);
+    const gameTrailersRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}/movies`
+    );
+    const gameScreenshotsRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}/screenshots`
+    );
+
+    return forkJoin({
+      gameInfoRequest,
+      gameScreenshotsRequest,
+      gameTrailersRequest,
+    }).pipe(
+      map((resp: any) => {
+        return {
+          ...resp['gameInfoRequest'],
+          screenshots: resp['gameScreenshotsRequest']?.results,
+          trailers: resp['gameTrailersRequest']?.results,
+        };
+      })
+    );
+  }
+
 }
